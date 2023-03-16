d344910f4265cddeeb8ae6127bd32644d73f4308
YuCh52 <91683271+YC-03@users.noreply.github.com>
2023-03-16 14:15:55 +0000

    Added Register Customer Tab

--- src/AdvisorMenu.java
+++ src/AdvisorMenu.java
@@ -1,2 +1,17 @@
-public class AdvisorMenu {
+import javax.swing.*;
+
+public class AdvisorMenu extends JFrame {
+    private JPanel adPlane;
+    private JTabbedPane TabMenu;
+    private JButton logoutButton;
+    private JTextField tfFirstName;
+    private JTextField tfLastName;
+    private JTextField tfAddress;
+    private JTextField tfEmail;
+    private JButton registerButton;
+    private JButton resetButton;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
