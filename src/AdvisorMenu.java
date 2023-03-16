4bba789b72f3f4fda28342a04f06e818d464f2f2
essat20 <91886526+essat20@users.noreply.github.com>
2023-03-16 17:40:55 +0000

    completed GUI form for advisor

--- src/AdvisorMenu.java
+++ src/AdvisorMenu.java
@@ -4,12 +4,24 @@ public class AdvisorMenu extends JFrame {
     private JPanel adPlane;
     private JTabbedPane TabMenu;
     private JButton logoutButton;
-    private JTextField tfFirstName;
-    private JTextField tfLastName;
-    private JTextField tfAddress;
-    private JTextField tfEmail;
+    private JTextField firstNameTextField;
+    private JTextField lastNameTextField;
+    private JTextField addressTextField;
+    private JTextField emailTextField;
     private JButton registerButton;
     private JButton resetButton;
+    private JLabel searchCustomerLabel;
+    private JTextField searchCustomerTextField;
+    private JButton searchCustomerButton;
+    private JTextField ticketTypeTextField;
+    private JTextField customerTypeTextField;
+    private JTextField commissionAmountTextField;
+    private JTextField reportTypeTextField;
+    private JTextField recordPaymentTextField;
+    private JTextField recordDiscountAmountTextField;
+    private JButton registerTicketButton;
+    private JButton createIndividualReportButton;
+    private JLabel registerTicketLabel;
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
