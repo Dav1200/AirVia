a51ba2394c4199021dea616d04d34ce5b446a6fe
Chakell <91855184+Chakell@users.noreply.github.com>
2023-03-16 00:05:35 +0000

    Tried to add a table which reads off the database on phpmyadmin, but does not work yet. But if data is manually inputted, it works

--- src/AdminMenu.java
+++ src/AdminMenu.java
@@ -1,11 +1,16 @@
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.*;
 
 public class AdminMenu extends JFrame{
-    public AdminMenu() {
+    public AdminMenu() throws SQLException, ClassNotFoundException {
 
-        workButton.addActionListener(new ActionListener() {
+        //manual input
+        //createTable();
+
+                workButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 Login a = new Login();
@@ -35,5 +40,57 @@ public class AdminMenu extends JFrame{
     private JTable DB;
 
 
+    //manual input
+    /*
+    private void createTable(){
+        Object[][] data = {{111, "Frank", "Furter", "frankfurter@ttechttonic.com", "17 Church Road, Blackpool", "Travel advisor", "adv123"},
+                {112, "Holly", "Harper", "hollyharper@ttechttonic.com", "44 Victoria Road, London", "Travel advisor", "adv123"},
+                {113, "India", "Iris", "indiairis@ttechttonic.com", "51 Green Lane, Birmingham", "Travel advisor", "adv123"}};
+        DB.setModel(new DefaultTableModel(data, new String[]{"StaffID", "Firstname", "Lastname", "Email", "Address", "Role", "Password"}));
+    }
+
+     */
+
+    //automated input
+
+    //get connection
+    try(Connection con = DBConnection.getConnection();)
+
+    {
+
+    Statement stm = con.createStatement();
+    String query = "SELECT * FROM `STAFF`";
+    ResultSet resultSet = stm.executeQuery(query);
+    ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+    DefaultTableModel model = (DefaultTableModel) DB.getModel();
+
+    //getting column names
+    int col = resultSetMetaData.getColumnCount();
+    String[] colName = new String[col];
+    for(int i = 1; i< col; i++){
+        colName[i] = resultSetMetaData.getColumnName(col);
+
+    }
+    model.setColumnIdentifiers(colName);
+
+    //getting data
+    String StaffID, Firstname, Lastname, Email, Address, Password;
+    while(resultSet.next()) {
+        StaffID = resultSet.getString(1);
+        Firstname = resultSet.getString(1);
+        Lastname = resultSet.getString(1);
+        Email = resultSet.getString(1);
+        Address = resultSet.getString(1);
+        Password = resultSet.getString(1);
+
+        String[] row = {StaffID, Firstname, Lastname, Email, Address, Password};
+        model.addRow(row);
+    }
+    catch(ClassNotFoundException | SQLException e){
+            System.out.println(e);
+
+        }
+
+    }
 
 }
